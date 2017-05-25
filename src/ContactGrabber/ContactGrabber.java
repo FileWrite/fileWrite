88aec81a2b43cb17bd4a973f9a0bcd27eef5cf0d
user <mramirez@usa.com>
2017-05-25 17:40:24 -0500

    contactGrabber file setup

--- src/ContactGrabber/ContactGrabber.java
+++ src/ContactGrabber/ContactGrabber.java
@@ -5,32 +5,84 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
+import java.util.Scanner;
 
 
 public class ContactGrabber {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
 
-    String directory = "data";
-    String filename = "info.txt";
+        String directory = "data";
+        String filename = "contactNames.txt";
 
-    Path dataDirectory = Paths.get(directory);
-    Path dataFile = Paths.get(directory, filename);
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
 
-    if (Files.notExists(dataDirectory)) {
-        Files.createDirectories(dataDirectory);
-    }
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
 
-    if (! Files.exists(dataFile)) {
-        Files.createFile(dataFile);
-    }
-    List<String> lines =Files.readAllLines(dataFile);
-    for(String line : lines){
-        System.out.println(line);
-    }
-//    Path Files.write(Path filepath, List<String> lines[, StandardOpenOption option])
+        if (!Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        List<String> contacts = Files.readAllLines(dataFile);
+
+        do {
+
+            System.out.println("---------------------");
+            System.out.println("1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n"+
+                    "Enter an option (1, 2, 3, 4 or 5):\n");
+
+            Scanner userInput = new Scanner(System.in);
+            int input = userInput.nextInt();
+
+            if (input == 1) {    //---------------------
+                for (String line : contacts) {
+                    System.out.println(line);
+                }
+            } else if (input == 2) {   //-----------------
+
+                System.out.println("enter contact name: ");
+                String ContactName = userInput.next();
+
+                System.out.println("enter contact number");
+                String ContactNumber = userInput.next();
+
+                contacts.add(ContactName + " | " + ContactNumber + "\n");
+
+            } else if (input == 3) {
+                System.out.println("Enter name to search for\n-------------------");
+                String searchName = userInput.next();
+                Boolean found = false;
+
+                for(String contact : contacts) {
+                    if (contact.toLowerCase().contains(searchName.toLowerCase())) {
+                        System.out.print(searchName + "  ---> was found in this contact :" + contact);
+                        found=true;
+                    }
+                }
+                if (!found) {
+                    System.out.println("No result found for: " + searchName);
+                }
+
+                else if ( input ==5){
+                    Files.write(
+                            Paths.get("data","contactNames.txt"),contacts);
+                    System.exit(0);
+                } //end option 5
+
+                //else if (input ==4){
+                  //  System.out.println("you entered #4");
+                //}else System.out.println("enter a valid menu option");
+
+            }//end of choice #3
 
 
+        } while (true);
 
     }//end psvm
-}//end class
+}// end class
